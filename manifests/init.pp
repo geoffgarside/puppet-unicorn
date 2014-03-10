2b8ea52d24104290c16951f1efdc783193695594
Geoff Garside <geoff@geoffgarside.co.uk>
2014-03-10 13:32:47 +0000

    Purge ruby and rack dependencies for now.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,6 @@ class unicorn(
   $provider = 'gem',
 ) {
 
-  # The unicorn gem has prerequisites that requires building native extensions.
-  require ruby::dev
-  include rack
-
   package { 'unicorn':
     ensure   => $ensure,
     provider => $provider,
